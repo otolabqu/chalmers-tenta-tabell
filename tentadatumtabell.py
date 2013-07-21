7b3227916403b96365da2ae0a36b5e8b36d64738
Emil <emilbzm@gmail.com>
2013-07-21 19:24:40 +0200

    CSV format, Tableau support, multi-program functionality
    Added lots of things, primarily to support CSV output, for use with
    tableau and to get all the BSc programmes in one run.

--- tentadatumtabell.py
+++ tentadatumtabell.py
@@ -1,42 +1,45 @@
+
 # -*- coding: utf-8 -*-
-# Todo:  Add the V or H letter to indicate the location
-# Todo:  Add ical or gcal output
-# Todo:  Handle "Ges av inst"
-# Todo:  Make a version compatible with MSc programmes (only 2 years , any other difference?)
-# Todo:  Make the whole process automated for all the BSc and MSc programmes in Chalmers and output a complete webpage with all of those. Then publish it.
-# Todo:  Add to each tentainstans a tag specifying which programme it is part of. Then make a searchable database for all programmes
+# Todo: Add the V or H letter to indicate the location
+# Todo: Add ical or gcal output
+# Todo: Handle "Ges av inst"
+# Todo: Make a version compatible with MSc programmes (only 2 years , any other difference?)
+# Todo: Make the whole process automated for all the BSc and MSc programmes in Chalmers and output a complete webpage with all of those. Then publish it.
+# Todo: Add to each tentainstans a tag specifying which programme it is part of. Then make a searchable database for all programmes
+# Todo: (130721) make a figure/schema of the internal data strucutre used, for better abililty to develop further
+
 
 #-------------------------------------------------------------------------------
-# Name:        module2
+# Name: module2
 # Purpose:
-#   Get exam dates from Chalmers Studieportalen and output them in a more readable and planning-friendly form,
-#   in chronological order and all 3 years of the BSc program on one page.
-#   Plan your exam periods and re-exams easier with this tool.
-#   Tested on the TKDAT-pages but should work with any URL from studieportalen BSc programme.
+# Get exam dates from Chalmers Studieportalen and output them in a more readable and planning-friendly form,
+# in chronological order and all 3 years of the BSc program on one page.
+# Plan your exam periods and re-exams easier with this tool.
+# Tested on the TKDAT-pages but should work with any URL from studieportalen BSc programme.
 #
 #
 #
 #
 #
-#   En kurskod kommer alltid först. När vi läst in en kurskod kan vi läsa in en href som säger att här kommer kursnamnet.
-#   Sen kollar vi efter datumsträngar och lägger till dem i en dict med den aktuella kursen.
-#   Sen sparar vi allt och outputar fint
+# En kurskod kommer alltid först. När vi läst in en kurskod kan vi läsa in en href som säger att här kommer kursnamnet.
+# Sen kollar vi efter datumsträngar och lägger till dem i en dict med den aktuella kursen.
+# Sen sparar vi allt och outputar fint
 
-#   Hitta en kurskod och spara som AKTUELL kurskod i parsern.
-#   Samtidigt skapas ett nytt entry i dicten om det inte redan finns, för denna kurs
-#   Sedan extraheras kursens namn ur parsern
-#   Samt upp till 3 datum
-#   Sedan kommer nästa kurs, och repeat
+# Hitta en kurskod och spara som AKTUELL kurskod i parsern.
+# Samtidigt skapas ett nytt entry i dicten om det inte redan finns, för denna kurs
+# Sedan extraheras kursens namn ur parsern
+# Samt upp till 3 datum
+# Sedan kommer nästa kurs, och repeat
 
-#   När alla kurser är inlästa i en bra tabell kan vi outputta dem i en annan form. Det löser vi sen.
+# När alla kurser är inlästa i en bra tabell kan vi outputta dem i en annan form. Det löser vi sen.
 
 
-# Author:      natwei
+# Author: natwei
 #
-# Created:     06/04/2013
-# Copyright:   (c) natwei 2013
-# Licence:     <your licence>
-# Using python 3.x  with
+# Created: 06/04/2013
+# Copyright: (c) natwei 2013
+# Licence: <your licence>
+# Using python 3.x with
 # http://docs.python-requests.org/en/latest/
 # http://docs.python.org/2/library/htmlparser.html
 # to run this type in the console for example
@@ -52,7 +55,9 @@ class TentaInstans():
         self.date = None
         self.courseCode = None
         self.courseName = None
-    def __repr__(self):  #formatting for text output
+        self.grade = None
+
+    def __repr__(self): #formatting for text output
         return self.readableDate() +" " + self.fmem() + " " + self.courseCode+ " "+ self.courseName + " "
 
     def html (self, addDate):
@@ -60,7 +65,11 @@ class TentaInstans():
         datestring = self.readableDate() if addDate else ""
         return basestring.format(datestring, self.fmem(), self.courseCode, self.courseName)
 
-    def keyfunction (i1):  #key function for list sorting
+    def csv (self):  #added 130721 for csv output for use with Tableau
+        basestring = ' {}; {}; {}; {}; {}; {}'
+        return basestring.format (self.readableDate(), self.fmem(), self.progr, self.grade, self.courseCode, self.courseName)
+
+    def keyfunction (i1): #key function for list sorting
         return i1.date
     def readableDate (self):
         return self.date[0:4]+'-'+self.date[4:6]+'-'+self.date[6:8]#+' ' + self.fmem() + ' ' + self.date[10:]
@@ -90,6 +99,8 @@ class CourseEntry ():
         self.name = ""
         self.code = ""
         self.datumlista = []
+        self.year = 0 #added 130721 for more functionality
+        self.programme = "" #added 130721 for more functionality
 
     def __repr__(self):
         s = self.name + " " + self.code + " "
@@ -100,31 +111,31 @@ class CourseEntry ():
 
 class MyHTMLParser(HTMLParser):
         def isDate (self,text):
-            #format  '11/03-2013 em M'
-            text = text.lstrip().rstrip()   #remove trailing whitespaces
+            #format '11/03-2013 em M'
+            text = text.lstrip().rstrip() #remove trailing whitespaces
             if len(text) == 15:
                 #print ("found 15 character data: ", text)
                 day = text[0:2]
                 month = text[3:5]
-                year  = text[6:10]
-                time  = text[11:13]
+                year = text[6:10]
+                time = text[11:13]
                 if day.isdigit() and month.isdigit() and year.isdigit() and time.isalpha():
-                    #print ("   found date ", text)
+                    #print (" found date ", text)
                     t = Tentadatum()
                     t.year = year
                     t.month = month
                     t.time= time
                     t.day = day
                     return t
-                return None  #not a valid date
-            return None  #not a 15 length string
+                return None #not a valid date
+            return None #not a 15 length string
 
 
         def isCourseCode ( self, text):
             #format 'DAT043'
             if len(text) == 6:
                 chars = text[0:3]
-                nums  = text[3:6]
+                nums = text[3:6]
                 if chars.isalpha() and nums.isdigit():
                     return True
 
@@ -133,7 +144,7 @@ class MyHTMLParser(HTMLParser):
             if tag == 'a':
                 #print (attrs)
                 for (k,v) in attrs:
-                    if  k == 'href':
+                    if k == 'href':
                         if 'course?course_id=' in v:
                             #print (k, " " , v)
                             self.findCourseName = True
@@ -147,6 +158,9 @@ class MyHTMLParser(HTMLParser):
                         self.courseCode = data
                         self.tabell[data] = CourseEntry ()
                         self.tabell[data].code = data
+                        self.tabell[data].grade = self.grade #added 130721
+                        self.tabell[data].progr = self.progr #added 130721
+
             if self.findCourseName: #registrera namnet och stäng sedan av denna sökfunktion
                         self.tabell[self.courseCode].name = data
                         self.findCourseName = False
@@ -154,42 +168,14 @@ class MyHTMLParser(HTMLParser):
             if t is not None:
                 self.tabell [self.courseCode].datumlista.append(t)
 
-def main():
+def printAsCSV (sorterad):
+    #behöver inte fixa datum, det kan tableau filtrera bort
+    #behöver inte ta bort fmem, det kan tableau använda sig av
+    print ("Datum; Tid; Programkod; År; Kurskod; Namn; ")  #CSV - tabellstruktur. ska sedan lägga till fler fält här
+    for x in sorterad:
+        print (x.csv())
 
-    datatabell = {}
-
-    parser = MyHTMLParser()
-    parser.courseCode = None
-    parser.findCourseName = False
-    parser.tabell = datatabell
-
-    #TO CREATE AN URL FOR PARSING:
-    #Get the URl from STUDIEPORTALEN for the BSc Programme year 1. Remove the grade number and split the string into two as follows:
-
-    #These URL's are examples, valid for the 2012/2013 year
-    stringparts = ['https://www.student.chalmers.se/sp/programplan?program_id=871&grade=','&conc_id=-1']   #TKDAT
-    #stringparts = ['https://www.student.chalmers.se/sp/programplan?program_id=875&grade=','&conc_id=-1']    #TKITE
-    #stringparts = ['https://www.student.chalmers.se/sp/programplan?program_id=904&grade=','&conc_id=-1']    #TKTEM
-    #stringparts = ['https://www.student.chalmers.se/sp/programplan?program_id=870&grade=','&conc_id=-1']   #TKBIO
-
-    for grade in ['1', '2', '3']:
-        url = grade.join (stringparts)
-        data = requests.get (url).text
-        parser.feed(data)
-
-    utdatatabell = []
-    for k,v in datatabell.items():
-        for d in v.datumlista:
-            if d.time == "em":
-                d.time = "kv"  #em kallas istället kväll, eftersom kv kommer efter fm, så om två tentor har samma datum kan string compare sortera dem rätt enligt "fm" "em"
-            i = TentaInstans()
-            i.date = d.year+d.month+d.day+d.time
-            i.courseCode = v.code
-            i.courseName = v.name
-            utdatatabell.append(i)
-
-
-    sorterad = sorted (utdatatabell, key = TentaInstans.keyfunction )
+def printAsTable (sorterad):
 
     now = datetime.datetime.now()
     now = str(now).replace('-', '')
@@ -212,5 +198,65 @@ def main():
                 print(x.html(False))
 
     print ("</table>")
+
+
+def makeUtdataFromIndata (datatabell, utdatatabell):
+
+    for k,v in datatabell.items():
+        for d in v.datumlista:
+            if d.time == "em":
+                d.time = "kv" #em kallas istället kväll, eftersom kv kommer efter fm, så om två tentor har samma datum kan string compare sortera dem rätt enligt "fm" "em"
+            i = TentaInstans()
+            i.date = d.year+d.month+d.day+d.time
+            i.courseCode = v.code
+            i.courseName = v.name
+            i.grade = v.grade  # added 130721 grade is ambiguous but means year -> 1,2,3 are the possible values
+            i.progr = v.progr # added 130721...
+            utdatatabell.append(i)
+
+def main():
+    datatabell = {}
+    parser = MyHTMLParser()
+    parser.courseCode = None
+    parser.findCourseName = False
+    parser.tabell = datatabell
+
+    #TO CREATE AN URL FOR PARSING:
+
+#START new functionality from 130721 for multi program
+    prgdict = {}
+    prgdict ['TKDAT'] = 871
+    prgdict ['TKITE'] = 875
+    prgdict ['TKTEM'] = 904
+    prgdict ['TKBIO'] = 870
+    prgdict ['TKARK'] = 868
+    prgdict ['TKATK'] = 829
+    prgdict ['TKAUT'] = 869
+    prgdict ['TKELT'] = 873
+    prgdict ['TKIEK'] = 874
+    prgdict ['TKKEF'] = 876
+    prgdict ['TKKMT'] = 877
+    prgdict ['TKMAS'] = 878
+    prgdict ['TKDES'] = 872
+    prgdict ['TKTFY'] = 879
+    prgdict ['TKVOV'] = 880
+
+    urllist = []
+    for p in prgdict:
+        for grade in ['1', '2', '3']:
+            url = 'https://www.student.chalmers.se/sp/programplan?program_id={}&grade={}&conc_id=-1'.format (prgdict[p], grade)
+            data = requests.get (url).text
+            parser.grade = grade #added 130721 for Tableau. Not the best way to do it, but will work
+            parser.progr = p     #added 130721, same as above
+            parser.feed(data)
+
+#end new functionality for multiprogram
+
+    utdatatabell = []
+    makeUtdataFromIndata(datatabell, utdatatabell)
+    sorterad = sorted (utdatatabell, key = TentaInstans.keyfunction )
+    #printAsTable(sorterad) #For HTML
+    printAsCSV (sorterad)  #For use with Tableau
+
 if __name__ == '__main__':
     main()
